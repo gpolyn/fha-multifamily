97f29e7f999eddd54dd48eec7bbcc8accef4bd65
Gallagher Polyn <gallagher.polyn@gmail.com>
2011-12-19 18:24:37 -0500

    controller, views and functional tests for documentation static pages

---
+++ spec/controllers/api/beta1/documentation_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe Api::Beta1::DocumentationController, "#sec223f_refinance" do
+  it "should have expected route", :type=>:routing do
+    assert_routing({:path=>"api/beta1/documentation/sec223f/refinance", :method =>:get}, 
+                   {:controller=>"api/beta1/documentation", :action =>"sec223f_refinance"})
+  end
+  
+  describe "success" do
+    it "should be a success" do
+      get :sec223f_refinance
+      response.should be_success
+    end
+  end
+end
+
+describe Api::Beta1::DocumentationController, "#sec223f_acquisition" do
+  it "should have expected route", :type=>:routing do
+    assert_routing({:path=>"api/beta1/documentation/sec223f/acquisition", :method =>:get}, 
+                   {:controller=>"api/beta1/documentation", :action =>"sec223f_acquisition"})
+  end
+  
+  describe "success" do
+    it "should be a success" do
+      get :sec223f_acquisition
+      response.should be_success
+    end
+  end
+end
+
+describe Api::Beta1::DocumentationController, "#sec223f" do
+  it "should have expected route", :type=>:routing do
+    assert_routing({:path=>"api/beta1/documentation/sec223f", :method =>:get}, 
+                   {:controller=>"api/beta1/documentation", :action =>"sec223f"})
+  end
+  
+  describe "success" do
+    it "should be a success" do
+      get :sec223f
+      response.should be_success
+    end
+  end
+end
